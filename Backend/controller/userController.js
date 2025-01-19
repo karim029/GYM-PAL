559c33e9fa8b126ca92e6184a639c58795a5ce37
karim <karim.tarek96@yahoo.com>
2025-01-19 18:15:21 +0200

    added login logic

--- Backend/controller/userController.js
+++ Backend/controller/userController.js
@@ -1,15 +1,31 @@
 const User = require("../model/userModel");
+const jwt = require("jsonwebtoken");
 
+const createToken = (_id) => {
+  return jwt.sign({ _id }, process.env.JWT_SECRET, { expiresIn: "3d" });
+};
 // login user
 const loginUser = async (req, res) => {
-  res.json({ message: "Login user" });
+  const { email, password } = req.body;
+  try {
+    const user = await User.login(email, password);
+
+    // create jwt token
+    const token = createToken(user._id);
+    res.status(200).json({ email, userId: user._id, token: token });
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
 };
 //signup user
 const signUpUser = async (req, res) => {
   const { email, password } = req.body;
   try {
     const user = await User.signup(email, password);
-    res.status(200).json({ email, userId: user._id });
+
+    // create jwt token
+    const token = createToken(user._id);
+    res.status(200).json({ email, userId: user._id, token: token });
   } catch (error) {
     res.status(400).json({ error: error.message });
   }
